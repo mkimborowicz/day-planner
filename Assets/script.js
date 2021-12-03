844a16cd7663ab7f68fb478b38ed9f5002e6959e
maddy <maddykimborowicz@gmail.com>
2021-12-03 12:52:30 -0800

    finished planner layout and started local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,30 +1,38 @@
-var schedule = $(".containter");
+var schedule = $(".container");
 var currentDay = $("#currentDay");
 var index = 0;
 var today = moment();
-var hour = today.format("hh:mm a");
+var currentTime = today.format("hh:mm a");
 currentDay.text(today.format("dddd, MMMM Do YYYY"));
 
 var hours = [
-  "9:00 AM",
-  "10:00 AM",
-  "11:00 AM",
-  "12:00 PM",
-  "1:00 PM",
-  "2:00 PM",
-  "3:00 PM",
-  "4:00PM",
-  "5:00 PM",
+  "9 AM",
+  "10 AM",
+  "11 AM",
+  "12 PM",
+  "1 PM",
+  "2 PM",
+  "3 PM",
+  "4 PM",
+  "5 PM",
 ];
 
 for (i = 9; i < 18; i++, index++) {
-  $("#hour-" + [i]).text(hours[index]);
-  $("#hour-" + [i]).append("<textarea></textarea>");
-  $("#hour-" + [i]).append("<button></button>");
+  $("#hour-" + [i]).append("<p></p>");
+  $("#hour-" + [i]).children("p").text(hours[index]);
+  $("#hour-" + [i]).append("<textarea></textarea>")
+  $("#hour-" + [i]).append("<button>save</button>");
 
+  var toDoItem = localStorage.getItem("hour-" + [index]);
+  var textArea = $("textarea");
+  textArea.text(toDoItem);
   
+  schedule.on("click", "button",function(event){
+    localStorage.setItem("hour-" + [index], textArea.val())
+  });
 
-  var eventsByHour = localStorage.getItem("hour-" + i);
 }
 
-schedule.on("click", "saveBtn", function (event) {});
+schedule.children("div").children("button").attr("class", "saveBtn");
+schedule.children().attr("class", "row");
+schedule.children("div").children("p").attr("class", "hour");
